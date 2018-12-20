f6276b9d91b32028f1e03451d77d93d3da017bde
unknown <trandrew@mail.fresnostate.edu>
2018-12-20 14:38:30 -0800

    Lab-End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
+
+// angular material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialDesignModule} from './material-design/material-design.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { MainFooterComponent } from './main-footer/main-footer.component';
     MainFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
