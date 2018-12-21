19d4f079c892130204be0d4055798356a4743f29
unknown <trandrew@mail.fresnostate.edu>
2018-12-20 21:03:17 -0800

    finished lab5

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-interface IStudent {
-  id?: number;
-  firstName: string;
-  lastName: string;
-  course: string;
-}
-
 @Component({
   selector: 'main-content',
   templateUrl: './main-content.component.html',
@@ -14,57 +7,9 @@ interface IStudent {
 })
 export class MainContentComponent implements OnInit {
 
-
-  students: Array<IStudent> = [];
-
-editMode: boolean = false;
   constructor() {
-    this.students[0] = {
-      id: 1,
-      firstName: 'Lucas',
-      lastName: 'Phan',
-      course: 'Programming'
-    }
-    this.students[1] = {
-      id: 1,
-      firstName: 'Tom',
-      lastName: 'Brady',
-      course: 'Swimming'
-    }
-    this.students[2] = {
-      id: 1,
-      firstName: 'Mark',
-      lastName: 'Zuc',
-      course: 'Business'
-    }
-    this.students[3] = {
-      id: 1,
-      firstName: 'Joe',
-      lastName: 'Biden',
-      course: 'Politics'
-    }
+
   }
 
-  ngOnInit() {
-  }
-
-  addStudent() {
-    this.editMode = true;
-    const student: IStudent = {
-      id: null,
-      firstName: null,
-      lastName: null,
-      course: null,
-    };
-
-    //this.students.push(student);
-    this.students.unshift(student);
-  }
-removeStudent(index:number) {
-  console.log('from removeStudent function index:', index);
-  this.students.splice(index, 1)
-}
-saveStudent() {
-  this.editMode = false;
-}
+  ngOnInit() {};
 }
--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'main-header',
   templateUrl: './main-header.component.html',
@@ -10,9 +10,13 @@ export class MainHeaderComponent implements OnInit {
   private firstName: string;
   private lastName: string;
 
-  constructor() {
+  constructor(private router: Router) {
     this.firstName = 'Lucas';
     this.lastName = 'Phan';
+  }
+
+  goToPage(path: string) {
+    this.router.navigate([path]);
   }
 
   showGreeting() {
@@ -23,9 +27,7 @@ export class MainHeaderComponent implements OnInit {
   hideGreeting() {
     const banner = document.getElementById('banner');
     banner.innerHTML = 'r1c2';
-
   }
-
 
   ngOnInit() {
   }
